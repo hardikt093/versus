7a6232fafa2db0deb736b07d9a6a376da1c96133
krimy <krimys.albiorix@gmail.com>
2023-04-28 11:07:20 +0530

    file chnages in league repo for socket

--- league/source/sockets/index.ts
+++
@@ -1,8 +0,0 @@
-import { disconnect } from "./chat.service";
-
-export default (socket: any) => {
-  const myId = socket.handshake.query.userId;
-  // connection(Number(myId), socket);
-
-  socket.on("disconnect", () => disconnect(Number(myId), socket));
-};
