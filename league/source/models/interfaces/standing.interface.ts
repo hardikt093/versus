a5c5836ebb967d9b514efe7eaf124eeffde8d109
nidhi007albiorix <106663088+nidhi007albiorix@users.noreply.github.com>
2023-05-10 18:32:01 +0530

    Added interface for standing

---
+++ league/source/models/interfaces/standing.interface.ts
@@ -0,0 +1,21 @@
+import { Document } from "mongoose";
+export interface IStanding extends Document {
+  leagueId: String;
+  leagueType: String;
+  goalServeLeagueId: Number;
+  division: String;
+  away_record: String;
+  current_streak: String;
+  games_back: String;
+  home_record: String;
+  teamId: String;
+  goalServePlayerId:Number;
+  lost: String;
+  name: String;
+  position: String;
+  runs_allowed: String;
+  runs_diff: String;
+  runs_scored: String;
+  won: String;
+}
+export default IStanding;
