1ed657ca1a45e7f395c314c5e53ce3731d8da080
Ankur <ankurakv1793@gmail.com>
2023-05-12 13:33:40 +0530

    odds model updated

--- league/source/models/interfaces/odd.interface.ts
+++ league/source/models/interfaces/odd.interface.ts
@@ -1,14 +1,20 @@
 import { Document } from "mongoose";
+    type moneyLine = {
+      dp3: string,
+      id: string,
+      name: string,
+      us: string,
+      value: string,
+    }
 export interface IOddModel extends Document {
-  leagueId: String;
-  matchId: String;
-  homeTeamId: String;
-  awayTeamId: String;
-  homeTeamMoneyLine: String;
-  homeTeamSpread: String;
-  homeTeamRunLine: String;
-  awayTeamMoneyLine: String;
-  awayTeamSpread: String;
-  awayTeamRunLine: String;
+    goalServerLeagueId: string,
+    goalServerMatchId: string,
+    goalServerHomeTeamId: string,
+    goalServeAwayTeamId: string,
+    homeTeamSpread: string,
+    homeTeamRunLine: string,
+    awayTeamSpread: string,
+    awayTeamRunLine: string,
+    moneyLine : Array<moneyLine>
 }
 export default IOddModel;
