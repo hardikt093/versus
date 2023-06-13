3c364154e69d80527c6a923c8a4a5334b614b63d
krimy <krimys.albiorix@gmail.com>
2023-06-13 18:53:08 +0530

    changes in null value of spread

--- league/source/goalserve/NHL/nhl.service.ts
+++ league/source/goalserve/NHL/nhl.service.ts
@@ -4432,11 +4432,11 @@ const createAndUpdateOddsNhl = async () => {
           );
           const awayTeamSpread = getAwayTeamRunLine
             ? getAwayTeamRunLine?.name?.split(" ").slice(-1)[0]
-            : "null";
+            : "";
 
           const homeTeamSpread = getHomeTeamRunLine
             ? getHomeTeamRunLine?.name?.split(" ").slice(-1)[0]
-            : "null";
+            : "";
           const total = await getTotal("Over/Under", matchData?.odds?.type);
           const totalValues = await getTotalValues(total);
           let data = {
