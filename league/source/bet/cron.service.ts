53072c9afdc86ebddd12395b4b14d5fb81dde3d1
krimy <krimys.albiorix@gmail.com>
2023-07-11 17:48:33 +0530

    added refund in tie match

--- league/source/bet/cron.service.ts
+++ league/source/bet/cron.service.ts
@@ -97,6 +97,24 @@ export default class BetDbCronServiceClass {
       for (let i = 0; i < betData.length; i++) {
         const bet = betData[i];
         // Please Do refund Code below
+        const resp = await axiosPostMicro(
+          {
+            amount: bet?.requestUserBetAmount,
+            userId: bet?.requestUserId,
+            betData: bet
+          },
+          `${config.authServerUrl}/wallet/revertAmount`,
+          ""
+        );
+        const resp2 = await axiosPostMicro(
+          {
+            amount: bet?.opponentUserBetAmount,
+            userId: bet?.opponentUserId,
+            betData: bet
+          },
+          `${config.authServerUrl}/wallet/revertAmount`,
+          ""
+        );
         
         // update payment Status after payment refunded
         await Bet.updateOne(
