3e8ab6e7a8b1aefc467bbaa22ebc907f4e7fbf7e
Nidhi <nidhib.albiorix@gmail.com>
2023-07-17 18:08:52 +0530

    commented contact related code

--- chat/source/contact/contact.validation.ts
+++ chat/source/contact/contact.validation.ts
@@ -1,8 +1,8 @@
-import Joi from "@hapi/joi";
+// import Joi from "@hapi/joi";
 
-const createContact = {
-  body: Joi.object().keys({
-    email: Joi.string().required(),
-  }),
-};
-export default { createContact };
+// const createContact = {
+//   body: Joi.object().keys({
+//     email: Joi.string().required(),
+//   }),
+// };
+// export default { createContact };
