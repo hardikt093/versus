b61478492db569de6dce5b11ddcf6bd49586d36e
krimy <krimys.albiorix@gmail.com>
2023-07-18 17:10:29 +0530

    - added decryption code in chat - added social login if user does not have contact

---
+++ chat/source/services/crypto.service.ts
@@ -0,0 +1,12 @@
+import CryptoJS from 'crypto-js';
+import config from '../config/config';
+
+const decryptMessage = async (text: string) => {
+    try {
+        return CryptoJS.AES.decrypt(text, config.message_key).toString(CryptoJS.enc.Utf8);
+    } catch (error: any) {
+        console.log("error", error)
+    }
+}
+
+export default { decryptMessage }
