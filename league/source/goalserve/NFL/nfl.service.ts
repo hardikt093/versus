6deedd7a5b00bdec357e66326749474e205c88c6
abdul <abduls.albiorix@gmail.com>
2023-08-07 19:42:54 +0530

    added standing api call in add team

--- league/source/goalserve/NFL/nfl.service.ts
+++ league/source/goalserve/NFL/nfl.service.ts
@@ -1,3 +1,22 @@
-const addTeam = async () => {};
+import { goalserveApi } from "../../services/goalserve.service";
+
+const addTeam = async () => {
+  let data = {
+    json: true,
+  };
+  const getStandings = await goalserveApi(
+    "https://www.goalserve.com/getfeed",
+    data,
+    `football/nfl-standings`
+  );
+
+  //   console.log("getAwayTeamImage", getStandings.data.standings.category.league);
+  const getData = await getStandings.data.standings.category.league.map(
+    (item: any) => {
+      console.log("item=====>", item);
+    }
+  );
+  return getStandings.data;
+};
 
 export default { addTeam };
