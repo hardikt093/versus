dfb0979ec3f14ad8c69875ad4ebc7cf8c566fa00
Nidhi <nidhib.albiorix@gmail.com>
2023-08-24 17:17:47 +0530

    added one change in dbCron for adding standing in ncaaf

--- league/source/goalserve/NCAAF/db.cron.service.ts
+++ league/source/goalserve/NCAAF/db.cron.service.ts
@@ -19,7 +19,7 @@ export default class NCAAFDbCronServiceClass {
     });
     await Promise.all(
       getstanding?.data?.standings?.category?.league?.map(async (item: any) => {
-        await Promise.all(
+        if (item.division.length) {
           item.division.map(async (div: any) => {
             await Promise.all(
               div.team.map(async (team: any) => {
@@ -41,6 +41,7 @@ export default class NCAAFDbCronServiceClass {
                   position: team.position,
                   streak: team.streak,
                 };
+
                 await NCAAFStandings.findOneAndUpdate(
                   { goalServeTeamId: team?.id },
                   { $set: data },
@@ -48,8 +49,36 @@ export default class NCAAFDbCronServiceClass {
                 );
               })
             );
-          })
-        );
+          });
+        } else {
+          await Promise.all(
+            item.division.team.map(async (team: any) => {
+              let data = {
+                leagueId: league?._id,
+                leagueType: item?.name,
+                goalServeLeagueId: getstanding?.data?.standings?.category?.id,
+                division: item.division?.name,
+                goalServeTeamId: team?.id,
+                conference_lost: team.conference_lost,
+                conference_points_against: team.conference_points_against,
+                conference_points_for: team.conference_points_for,
+                conference_won: team.conference_won,
+                name: team.name,
+                overall_lost: team.overall_lost,
+                overall_points_against: team.overall_points_against,
+                overall_points_for: team.overall_points_for,
+                overall_won: team.overall_points_for,
+                position: team.position,
+                streak: team.streak,
+              };
+              await NCAAFStandings.findOneAndUpdate(
+                { goalServeTeamId: team?.id },
+                { $set: data },
+                { upsert: true }
+              );
+            })
+          );
+        }
       })
     );
   };
