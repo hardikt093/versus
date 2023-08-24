07db88260415011c4407b99b176f0420c9347c2c
Nidhi <nidhib.albiorix@gmail.com>
2023-08-24 19:10:00 +0530

    some changes in api to add teams in NCAAF

--- league/source/goalserve/NCAAF/ncaaf.service.ts
+++ league/source/goalserve/NCAAF/ncaaf.service.ts
@@ -1,22 +1,27 @@
 import TeamNCAAF from "../../models/documents/NCAAF/team.model";
+import League from "../../models/documents/league.model";
+import ILeagueModel from "../../models/interfaces/league.interface";
 var csv = require("csvtojson");
 
 const addTeam = async (data: any) => {
+  const league: ILeagueModel | undefined | null = await League.findOne({
+    goalServeLeagueId: 2,
+  });
   csv()
     .fromFile(data.path)
-    .then(async(jsonObj: any) => {
+    .then(async (jsonObj: any) => {
       var teamArray: any = [];
       for (var i = 0; i < jsonObj.length; i++) {
         var obj: any = {};
         obj.goalServeTeamId = Number(jsonObj[i]["id"]);
         obj.name = jsonObj[i]["Team"];
         obj.teamName = jsonObj[i]["Team Name"];
-        obj.leagueId = "64e6f19532c8ff1b335de377";
-        obj.leagueName = "FBS (Division I-A)";
+        obj.leagueId = league?._id;
+        obj.leagueName = league?.name;
         obj.division = jsonObj[i]["Division"];
         obj.locality = jsonObj[i]["Locality"];
         obj.isDeleted = false;
-        obj.goalServeLeagueId = 2;
+        obj.goalServeLeagueId = league?.goalServeLeagueId;
         obj.conference = jsonObj[i]["Conference"];
         obj.conferenceName = jsonObj[i]["Conference Name"];
         obj.conferenceId = jsonObj[i]["Conf_id"];
@@ -30,7 +35,6 @@ const addTeam = async (data: any) => {
     });
 };
 
-
 export default {
-  addTeam
+  addTeam,
 };
