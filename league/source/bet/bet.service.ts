b46f3ac1a72e176925bbedf6cea0e30f01288ddd
Nidhi <nidhib.albiorix@gmail.com>
2023-09-15 15:16:05 +0530

    sorting in api issue fixed

--- league/source/bet/bet.service.ts
+++ league/source/bet/bet.service.ts
@@ -1950,25 +1950,6 @@ const listBetsDashboard = async (body: IlistBetRequestData) => {
       },
     },
     {
-      $addFields: {
-        dateutc: {
-          $toDate: "$activeTimestamp",
-        },
-      },
-    },
-    {
-      $sort: {
-        dateutc: -1,
-      },
-    },
-    {
-      $addFields: {
-        dateInString: {
-          $toString: "$dateutc",
-        },
-      },
-    },
-    {
       $facet: {
         mlbData: [
           {
@@ -2634,7 +2615,26 @@ const listBetsDashboard = async (body: IlistBetRequestData) => {
         path: "$match",
         preserveNullAndEmptyArrays: true,
       },
-    }
+    },
+    {
+      $addFields: {
+        dateutc: {
+          $toDate: "$activeTimestamp",
+        },
+      },
+    },
+    {
+      $sort: {
+        dateutc: -1,
+      },
+    },
+    {
+      $addFields: {
+        dateInString: {
+          $toString: "$dateutc",
+        },
+      },
+    },
   );
   if (!body.socketType) {
     query.push(
@@ -2653,6 +2653,7 @@ const listBetsDashboard = async (body: IlistBetRequestData) => {
       goalServeMatchId: 1,
       requestUserId: 1,
       opponentUserId: 1,
+      dateutc:1,
       isSquared: { $ifNull: ["$isSquared", false] },
       betTotalAmount: { $round: ["$betTotalAmount", 2] },
       requestUserBetAmount: { $round: ["$requestUserBetAmount", 2] },
