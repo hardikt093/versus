c9da6e309521d419df0e01fb66f16a11a1069115
Nidhi <nidhib.albiorix@gmail.com>
2023-09-18 15:41:39 +0530

    added put

--- authentication/source/routes/user.route.ts
+++ authentication/source/routes/user.route.ts
@@ -125,5 +125,5 @@ router.post("/friends", auth, userController.userContacts);
 router.post("/list", auth, userController.usersList);
 router.post("/getBulk", userController.usersGetBulk);
 router.get("/getFriendList", auth, userController.getFriendList);
-router.post("/updateVenmoUserName", auth, userController.updateVenmoName)
+router.put("/updateVenmoUserName", auth, userController.updateVenmoName)
 export default router;
