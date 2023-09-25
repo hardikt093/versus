360a5554ed7f8e99c8dd7bafe06fcc7a1d5fe965
krimy <krimys.albiorix@gmail.com>
2023-09-25 12:05:44 +0530

    added dev.app.versus-social.com in server file

--- league/source/server.ts
+++ league/source/server.ts
@@ -57,7 +57,8 @@ export const io = new Server(httpServer, {
       "https://localhost:3000",
       "http://localhost:3001",
       "http://192.168.1.15:3000",
-      "https://app.versus-social.com"
+      "https://app.versus-social.com",
+      "https://dev.app.versus-social.com"
     ],
     credentials: true,
   },
