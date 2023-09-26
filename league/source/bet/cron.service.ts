b9457be2cd53c2acb86b9f5461dc7a7324d4da0f
Nidhi <nidhib.albiorix@gmail.com>
2023-09-26 08:38:07 +0530

    added logs in bet file

--- league/source/bet/cron.service.ts
+++ league/source/bet/cron.service.ts
@@ -60,9 +60,11 @@ export default class BetDbCronServiceClass {
               );
             }
           } else if (bet?.leagueType === "NFL") {
+            console.log("inside nfl")
             const getMatch = await NflMatch.findOne({
               goalServeMatchId: bet.goalServeMatchId,
             });
+            console.log("getMatch nfl", getMatch?.goalServeMatchId)
             const totalRunOfMAtch =
               Number(getMatch?.awayTeamTotalScore) +
               Number(getMatch?.homeTeamTotalScore);
@@ -78,7 +80,10 @@ export default class BetDbCronServiceClass {
                 : bet?.requestUserGoalServeOdd.includes("U")
                 ? bet?.requestUserId
                 : bet?.opponentUserId;
+              
             if (oddWin === bet?.requestUserId) {
+              console.log("inside request user payment");
+              
               // payout to req user
               const resp = await axiosPostMicro(
                 {
@@ -90,6 +95,7 @@ export default class BetDbCronServiceClass {
                 ""
               );
             } else {
+              console.log("inside opponent user payment");
               // payout to opponent user
               const resp = await axiosPostMicro(
                 {
